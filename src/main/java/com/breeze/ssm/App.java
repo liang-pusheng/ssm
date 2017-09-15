1142f4f8633e4d11b21dc0aa2e74b9f78de01db5
breeze <934954360@qq.com>
2017-09-15 16:58:32 +0800

    SSM

---
+++ src/main/java/com/breeze/ssm/App.java
@@ -0,0 +1,13 @@
+package com.breeze.ssm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
